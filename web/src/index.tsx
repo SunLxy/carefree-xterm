0ab3137b5ccf4fb96200b7fa77cde255a3d4e073
SunLxy <1011771396@qq.com>
2022-11-03 21:26:11 +0800

    doc:添加 README

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,9 +1,14 @@
+/**
+ * 这个组件用于创建控制台
+ * 操作控制台
+ * 连接服务
+ * 执行命令
+*/
+
 import React from "react"
 
-
-const Xt = () => {
-
+const XtermConsole = () => {
   return <div>212</div>
 }
 
-export default Xt
+export default XtermConsole
